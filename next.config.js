e4c7b20960291398acea13a477da2f02467ee21c
Olajide Mathew <backend@yuniq.solutions>
2023-01-27 02:50:39 +0100

    prod

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://api.example.com/:path*',
+            },
+        ]
+    },
     async headers() {
         return [
             {
